0933b6f9778262b8b9586586b08ea0968a1b4662
Martijn Terpstra <bigmartijn@gmail.com>
2016-11-17 15:09:33 +0100

    Added automated tests, just run test.sh in aut-test/ directory
    each test $TEST is defined by three files
    
    $TEST_input.txt
    $TEST_output.txt
    $TEST_errors.txt
    
    the bash script test.sh runs each *input.txt file and compares (using
    the diff command) their stdout and sterr with the given *output.txt
    and *errors.txt. If they match the test is succesful, if not it fails
    and shows the diff of that output with the expected output.
    
    change or add files in the auto-test/usecase/ to change or add file
    new tests. Right now they tests executed are hardcode in test.sh so
    you have to add the names of the new test there if you want extra
    tests.

---
+++ auto-test/test.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/bash
+
+# Bash script om use cases te testen
+
+BASEDIR="$HOME/CommandlineCalculator/auto-test/"
+
+for testcase in exit\
+                    function_assignment\
+                    builtin_function1\
+                    builtin_function2\
+                    variable_assignment\
+                    builtin_variables\
+                    expression1\
+                    expression2\
+                    equality1\
+                    equality2\
+                    equality3
+do
+    echo -en ""$testcase": "
+
+    # pipe input and catch outputs and errors
+
+    cat $BASEDIR"usecases/"$testcase"_input.txt" |
+        java -jar $BASEDIR"../target/CommandlineCalculator-1.0-jar-with-dependencies.jar"\
+             > $BASEDIR"outputs/"$testcase"_output.txt"\
+             2> $BASEDIR"outputs/"$testcase"_errors.txt"
+
+    # compare stdout to expected stdout
+    if diff $BASEDIR"usecases/"$testcase"_output.txt" $BASEDIR"outputs/"$testcase"_output.txt";
+    then
+        echo -n "[OK]"
+    else
+        echo "*FAIL* on ["$testcase"] Output"
+        # show difference
+        diff $BASEDIR"usecases/"$testcase"_output.txt" $BASEDIR"outputs/"$testcase"_output.txt"
+    fi
+
+    # compare sterr to expected stderr
+    if diff $BASEDIR"usecases/"$testcase"_errors.txt" $BASEDIR"outputs/"$testcase"_errors.txt";
+    then
+        echo "[OK]"
+    else
+        echo "*FAIL* ["$testcase"] Error"
+        # show difference
+        diff $BASEDIR"usecases/"$testcase"_errors.txt" $BASEDIR"outputs/"$testcase"_errors.txt"
+    fi
+
+done
+
+# shell script itself didn't fail
+true
